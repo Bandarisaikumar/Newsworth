d086c39dcf3c50698bb27dd45965e0f90fa4d877
Bandarisaikumar <bandarisaikumar730@gmail.com>
2025-01-21 17:09:00 +0530

    minor changes done

--- app/src/main/java/com/example/newsworth/ui/view/fragment/RegistrationScreen.kt
+++ app/src/main/java/com/example/newsworth/ui/view/fragment/RegistrationScreen.kt
@@ -302,6 +302,7 @@ class RegistrationScreen : Fragment() {
                 }*/
             }
         }
+
         viewModel.otpResponse.observe(viewLifecycleOwner) { response ->
             response?.let {
                 handleOtpResponse(it)
